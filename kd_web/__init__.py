690fac620564d9703274253a2c03246966138e8a
Cem Aksoylar <caksoylar@users.noreply.github.com>
2024-07-10 20:16:35 -0700

    Add warning icon for active layout override

--- kd_web/__init__.py
+++ kd_web/__init__.py
@@ -253,7 +253,8 @@ def keymap_draw_row(need_rerun: bool):
                     anchor=False,
                 )
             with layout_col:
-                with st.popover("Layout override", use_container_width=True):
+                active_icon = ":warning: " if state.get("qmk_layout_file") else ""
+                with st.popover(active_icon + "Layout override", use_container_width=True):
                     st.write(
                         "You can override the physical layout spec description in Keymap YAML with a custom layout "
                         "description file here, similar to `qmk_info_json` option mentioned in the "
@@ -262,7 +263,7 @@ def keymap_draw_row(need_rerun: bool):
                     st.caption(
                         "Note: If there are multiple layouts under the `layouts` field, the first one will be used."
                     )
-                    qmk_layout_file = st.file_uploader(label="QMK `info.json`-format layout description", type=["json"])
+                    st.file_uploader(label="QMK `info.json`-format layout description", type=["json"], key="qmk_layout_file")
             with opts_col:
                 with st.popover("Draw filters", use_container_width=True):
                     draw_opts["draw_layers"] = st.multiselect(
@@ -281,7 +282,7 @@ def keymap_draw_row(need_rerun: bool):
                     except ValueError as err:
                         handle_exception(st, "Values must be space-separated integers", err)
 
-            layout_override = {"qmk_info_json": qmk_layout_file} if qmk_layout_file is not None else None
+            layout_override = {"qmk_info_json": state.qmk_layout_file} if state.get("qmk_layout_file") is not None else None
 
             svg = draw(keymap_data, draw_cfg, layout_override, **draw_opts)
 
